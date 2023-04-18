17fee7440712cd644847761cd42e568a6b8adfe9
rband709 <119666248+rband709@users.noreply.github.com>
2023-04-18 15:26:38 +0330

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    API_ID = int( os.getenv("api_id","3335796") )
+    API_HASH = os.getenv("api_hash","138b992a0e672e8346d8439c3f42ea78")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001621703668") )
+    CHANNEL_USERNAME = os.getenv("channel_username","Seriesplus1")
+    TOKEN = "5535287993:AAFvr3OJAbuMwKEvBDgB0nwIKbmwoLNqw1A"
+    DOMAIN  = os.getenv("domain","https://newfasterlink80.osc-fr1.scalingo.io")
